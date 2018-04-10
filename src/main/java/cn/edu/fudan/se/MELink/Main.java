1e2b5f7602684bcc94bbaf94c96debb1c94dc194
ruanhang1993 <ruanhang1993@hotmail.com>
2018-04-10 10:06:05 +0800

    test

--- src/main/java/cn/edu/fudan/se/MELink/Main.java
+++ src/main/java/cn/edu/fudan/se/MELink/Main.java
@@ -8,11 +8,11 @@ import cn.edu.fudan.se.MELink.webuild.CorpusBuilder;
 public class Main {
 
     public static void main(String[] args){
-//    	try {
-//			new CorpusBuilder("model.dat").build();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+    	try {
+			new CorpusBuilder("model.dat").build();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     	new StandfordParser().parse("I have bootstrapped this doc with things that should not be controversial, just statements of fact of what exists. If any are controversial, I apologize, please open a PR with the recommended change.");
     }
 }
