6b71791ab53c21e8ef4ae2c6922ebe81e7371a45
ruanhang1993 <ruanhang1993@hotmail.com>
2018-04-11 14:48:38 +0800

    update .gitignore

--- src/main/java/cn/edu/fudan/se/MELink/nlp/FileOperator.java
+++ src/main/java/cn/edu/fudan/se/MELink/nlp/FileOperator.java
@@ -8,17 +8,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import cn.edu.fudan.se.MELink.util.StringUtils;
+
 public class FileOperator {
 	private String toDealFile;
 	private String newFile;
+	private StandfordParser parser;
 	public FileOperator(String toDealFile, String newFile){
 		this.toDealFile = toDealFile;
 		this.newFile = newFile;
+		parser = new StandfordParser();
 	}
 	public void deal() throws IOException{
 		try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFile, true),"utf-8"));
 				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(toDealFile),"utf-8"))){
-			
+			br.lines().forEach(s->{
+				try {
+					bw.write(StringUtils.link(parser.parse(s)));
+					bw.write("\n");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
 		}
 	}
 }
--- src/main/java/cn/edu/fudan/se/MELink/util/StringUtils.java
+++ src/main/java/cn/edu/fudan/se/MELink/util/StringUtils.java
@@ -3,7 +3,7 @@ package cn.edu.fudan.se.MELink.util;
 import java.util.List;
 
 public class StringUtils {
-	public static String changeWebsite2Path(List<String> list){
+	public static String link(List<String> list){
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < list.size()-1; i++)
 			sb.append(list.get(i)).append(" ");
