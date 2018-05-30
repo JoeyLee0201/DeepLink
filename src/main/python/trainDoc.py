3c3f6601bc97abd0da38e9076e14e33a4db6b23d
ruanhang1993 <ruanhang1993@hotmail.com>
2018-05-30 15:47:14 +0800

    使用doc2vec

--- src/main/python/trainDoc.py
+++ src/main/python/trainDoc.py
@@ -9,18 +9,18 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
-def getVecs(model, corpus, size):  
-    vecs = [np.array(model.docvecs[z.tags[0]].reshape(1, size)) for z in corpus]  
-    return np.concatenate(vecs)  
+# def getVecs(model, corpus, size):
+#     vecs = [np.array(model.docvecs[z.tags[0]].reshape(1, size)) for z in corpus]
+#     return np.concatenate(vecs)
 
 
-def testD():
-    model_dm = Doc2Vec.load("text.model")
-    test_text = ['fix', 'bug']  
-    inferred_vector_dm = model_dm.infer_vector(test_text)  
-    print inferred_vector_dm  
-    sims = model_dm.docvecs.most_similar([inferred_vector_dm], topn=10)  
-    return sims  
+# def testD():
+#     model_dm = Doc2Vec.load("text.model")
+#     test_text = ['fix', 'bug']
+#     inferred_vector_dm = model_dm.infer_vector(test_text)
+#     print inferred_vector_dm
+#     sims = model_dm.docvecs.most_similar([inferred_vector_dm], topn=10)
+#     return sims
   
 # if __name__ == '__main__':  
 #     print 'start train'
@@ -38,14 +38,20 @@ def testD():
 
 
 if __name__ == '__main__':  
-    print '=============== text start train ==============='
-    texts = TaggedLineDocument('corpus/text12983151.dat')
-    textModel = Doc2Vec(texts, min_count=1, dm=0, window=5, size=100, sample=1e-3, negative=5, workers=4)
-    textModel.save('text12983151.model')
-    print '=============== text end train ==============='
+    # print '=============== text start train ==============='
+    # texts = TaggedLineDocument('corpus/text12983151.dat')
+    # textModel = Doc2Vec(texts, min_count=1, dm=0, window=5, size=100, sample=1e-3, negative=5, workers=4)
+    # textModel.save('text12983151.model')
+    # print '=============== text end train ==============='
+    #
+    # print '=============== code start train ==============='
+    # codes = TaggedLineDocument('corpus/code12983151.dat')
+    # codeModel = Doc2Vec(codes, min_count=1, dm=0, window=5, size=100, sample=1e-3, negative=5, workers=4)
+    # codeModel.save('code12983151.model')
+    # print '=============== code end train ==============='
 
-    print '=============== code start train ==============='
-    codes = TaggedLineDocument('corpus/code12983151.dat')
-    codeModel = Doc2Vec(codes, min_count=1, dm=0, window=5, size=100, sample=1e-3, negative=5, workers=4)
-    codeModel.save('code12983151.model')
-    print '=============== code end train ==============='
+    print '=============== nocode start train ==============='
+    codes = TaggedLineDocument('test/nocode50904245.dat')
+    codeModel = Doc2Vec(codes, min_count=1, dm=0, window=5, size=200, sample=1e-3, negative=5, workers=4)
+    codeModel.save('test/nocodedoc50904245.model')
+    print '=============== nocode end train ==============='
