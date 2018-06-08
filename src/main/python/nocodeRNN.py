4234a9456a599c67f1b004e7a85dbffb10f819f7
ruanhang1993 <ruanhang1993@hotmail.com>
2018-06-08 15:24:40 +0800

    rnn finish

--- src/main/python/nocodeRNN.py
+++ src/main/python/nocodeRNN.py
@@ -1,9 +1,6 @@
 # -*- coding: UTF-8 -*-
-
-
 from gensim.models import word2vec
 from preprocessor import preprocessor
-from gensim.models.doc2vec import Doc2Vec
 
 import tensorflow as tf
 import numpy as np
@@ -16,13 +13,10 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 VECTOR_SIZE = 100
 TRAIN_ITERS = 1000
 BATCH_SIZE = 20
-# NUM_STEPS = 4
 HIDDEN_SIZE = 100
-# N_CLASSES = 2
 N_INPUTS = 100
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.001
 
-tf.get_default_graph()
 wordModel = word2vec.Word2Vec.load('test/nocode50904245.model')
 
 
@@ -43,17 +37,14 @@ def text2vec(text, isHtml):
 
 #  shape = [None, seq len, Vec size]
 def read_data(path='./train'):
-    # commit_text = ''
-    # issue_text = ''
-    # commit_max = 0  # 667
-    # issue_max = 0  # 413
     X1 = []
     X2 = []
     L1 = []
     L2 = []
     Y = []
     filelist = os.listdir(path)
-    for i in range(0, 1):
+    for i in range(0, len(filelist)):
+    # for i in range(0, 1):
         filepath = os.path.join(path, filelist[i])
         print "Loaded the file:", filepath
         if os.path.isfile(filepath):
@@ -127,28 +118,27 @@ def make_batches(data, batch_size):
     return list(zip(data_batches1_rs, data_batches2_rs, len_batches1, len_batches2, label_batches))
 
 
-input1 = tf.placeholder(tf.float32, [BATCH_SIZE, None, VECTOR_SIZE])
-input2 = tf.placeholder(tf.float32, [BATCH_SIZE, None, VECTOR_SIZE])
-len1 = tf.placeholder(tf.int32, [BATCH_SIZE, ])
-len2 = tf.placeholder(tf.int32, [BATCH_SIZE, ])
-target = tf.placeholder(tf.float32, [BATCH_SIZE, 1])
+input1 = tf.placeholder(tf.float64, [BATCH_SIZE, None, VECTOR_SIZE])
+input2 = tf.placeholder(tf.float64, [BATCH_SIZE, None, VECTOR_SIZE])
+len1 = tf.placeholder(tf.int64, [BATCH_SIZE, ])
+len2 = tf.placeholder(tf.int64, [BATCH_SIZE, ])
+target = tf.placeholder(tf.float64, [BATCH_SIZE, 1])
 
 
 def RNN(input_data, seq_len):
-    rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_SIZE)
-    rnn_cell = tf.nn.rnn_cell.MultiRNNCell([rnn_cell for _ in range(3)])
-    initial_state = rnn_cell.zero_state(BATCH_SIZE, dtype=tf.float32)
-    outputs, state = tf.nn.dynamic_rnn(rnn_cell, input_data, sequence_length=seq_len, initial_state=initial_state, dtype=tf.float32)
+    rnn_cell = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_SIZE) for _ in range(3)])
+    outputs, state = tf.nn.dynamic_rnn(rnn_cell, input_data, sequence_length=seq_len, dtype=tf.float64)
     return outputs, state
 
 
-with tf.variable_scope("commit"):
-    outputs1, _ = RNN(input1, len1)
-with tf.variable_scope("issue"):
-    outputs2, _ = RNN(input2, len2)
+# initializer = tf.random_uniform_initializer(-0.5, 0.5, dtype=tf.float32)
+with tf.variable_scope("commit", reuse=tf.AUTO_REUSE):
+    outputs1, states1 = RNN(input1, len1)
+with tf.variable_scope("issue", reuse=tf.AUTO_REUSE):
+    outputs2, states2 = RNN(input2, len2)
 
-newoutput1 = outputs1[:, -1, :]
-newoutput2 = outputs2[:, -1, :]
+newoutput1 = states1[-1].h
+newoutput2 = states2[-1].h
 
 
 #  |t - cossimilar(state1, state2)|
@@ -159,7 +149,8 @@ def getLoss(state1, state2, t):
     score = tf.div(pooled_mul_12, pooled_len_1 * pooled_len_2+1e-8, name="scores")  #  +1e-8 avoid 'len_1/len_2 == 0'
     score = tf.reshape(score, [BATCH_SIZE, 1])
     rs = t - score
-    return tf.abs(rs)
+    rs = tf.abs(rs)
+    return tf.reduce_mean(rs)
 
 
 # Define loss and optimizer
@@ -168,20 +159,28 @@ loss_op = getLoss(newoutput1, newoutput2, target)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE)
 train_op = optimizer.minimize(loss_op)
 
+# writer = tf.summary.FileWriter('log/graphlog', tf.get_default_graph())
+# writer.close()
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
 train_batches = make_batches(read_data(), BATCH_SIZE)
 test_batches = make_batches(read_data(path="./testset"), BATCH_SIZE)
 # Start training
 with tf.Session() as sess:
+    saver = tf.train.Saver()
     sess.run(init)
 
     for step in range(TRAIN_ITERS):
         print step, '>>>>>>>>>>>>>>>>'
         for x1, x2, l1, l2, y in train_batches:
             loss, _ = sess.run([loss_op, train_op], feed_dict={input1: x1, input2: x2, len1: l1, len2: l2, target: y})
+
         if step % 100 == 0:
+            temp = []
             for x1, x2, l1, l2, y in test_batches:
                 loss = sess.run([loss_op], feed_dict={input1: x1, input2: x2, len1: l1, len2: l2, target: y})
-                print np.reshape(loss, [BATCH_SIZE, ])
+                temp.append(loss[0])
+            print temp
+            print "At the step %d, the avg loss is %f" % (step, np.mean(np.array(temp)))
+    saver.save(sess, 'rnnmodel/sess.rnn', global_step=TRAIN_ITERS)
     print("Optimization Finished!")
